93d30b85120fd6d596b70c4e7b092d65a50fc74f
vvj1037 <joglekar.varun@gmail.com>
2017-04-03 18:41:06 +0530

    Added CSS to startup register page

--- startup/forms.py
+++ startup/forms.py
@@ -5,13 +5,18 @@ from django import forms
 
 class StartupUserForm(forms.ModelForm):
     #username is already defined passsword is viewable
-    password = forms.CharField(widget=forms.PasswordInput())
-
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+        
 
 class StartupProfileForm(forms.ModelForm):
+    startupName = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    stockName = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    #accno = forms.CharField(widget=forms.Select(attrs={'class' : 'form-control'}))
     class Meta:
         model = StartupProfile
         fields = ('aadhar', 'accno','startupName','shareCount','sharePrice','stockName')
